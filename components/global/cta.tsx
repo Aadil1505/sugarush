f824db7c4a02b5d59f95c5f4fa5b9125474ec1b5
Aadil M. Alli <chainedraptor@gmail.com>
2025-11-23 01:10:34 -0500

    update

--- components/global/cta.tsx
+++ components/global/cta.tsx
@@ -6,7 +6,7 @@ export default function CallToAction() {
         <section className="py-16 md:py-32">
             <div className="mx-auto max-w-5xl px-6">
                 <div className="text-center">
-                    <h2 className="text-balance text-4xl font-semibold lg:text-5xl">What Are You Waiting For?</h2>
+                    <h2 className="text-4xl font-semibold lg:text-5xl">What Are You Waiting For?</h2>
                     <p className="mt-4 text-primary">Get your Sugarush in.</p>
 
                     <div className="mt-12 flex flex-wrap justify-center gap-4">
